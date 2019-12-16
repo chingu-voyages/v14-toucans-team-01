a82631babeaecf18ac3bd2a5d5b47a6ccc591100
Duncan Bain <duncan@bain.io>
2019-12-16 22:28:31 +0000

    split search types into seperate functions

--- js/Api.js
+++ js/Api.js
@@ -1,34 +1,21 @@
 // define constants for API url and API authentication key
-const BASE_API_URL = 'https://api.spoonacular.com/';
 const API_KEY = '046d215a9f344c39ad5e3bf194063388';
 
-async function recipeSearch(requestURL, searchType, requestParams) {
+async function recipeSearchByName(recipeSearchParams) {
 
-    // create URL object and add the search type to the base URL
-    switch (searchType) {
-        case 'recipe_name':
-            query_url = new URL(requestURL + "recipes/search");
-            console.log("recipe search");
-            break;
-        case 'ingredient_search':
-            query_url = new URL(requestURL + "recipes/findByIngredients");
-            console.log("ingredient search");
-            break;
-    }
-
-    console.log(query_url);
+    apiQueryURL = new URL('https://api.spoonacular.com/recipes/search'); // create URL object with recipe search API url
+    apiQueryURL.searchParams.append('limitLicense', 'true');    // limit results to those with atrribution
+    apiQueryURL.searchParams.append('number', '20');    // limit results to 20 recipes 
+    apiQueryURL.searchParams.append('instructionsRequired', 'true');    // limit results to recipes with instructions
 
     // loop through search parameters and append to URL
-    for (let key in requestParams) {
-        query_url.searchParams.append(key, requestParams[key]);
+    for (let key in recipeSearchParams) {
+        apiQueryURL.searchParams.append(key, recipeSearchParams[key]);
     }
 
-    console.log(query_url);
-
-    
     // because fetch returns a promise, we add a keyword to wait until the promise is returned
-    let response = await fetch(query_url);
-    let result = await response.json(); //convert response into JSON
+    let apiResponse = await fetch(apiQueryURL);
+    let apiResult = await apiResponse.json(); //convert response into JSON
 
-    return result; // return the result
+    return apiResult; // return the result
 };
