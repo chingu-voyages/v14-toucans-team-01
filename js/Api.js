3af14ea8454d5fce8a63673e4303109da6b27266
Duncan Bain <duncan@bain.io>
2019-12-22 17:31:48 +0000

    added bulk recipe info search

--- js/Api.js
+++ js/Api.js
@@ -1,17 +1,15 @@
 // define constants for API url and API authentication key
 const API_KEY = '046d215a9f344c39ad5e3bf194063388';
 
-async function recipeSearchByName(recipeSearchParams) {
 
-    apiQueryURL = new URL('https://api.spoonacular.com/recipes/search'); // create URL object with recipe search API url
-    apiQueryURL.searchParams.append('limitLicense', 'true');    // limit results to those with atrribution
-    apiQueryURL.searchParams.append('number', '20');    // limit results to 20 recipes 
-    apiQueryURL.searchParams.append('instructionsRequired', 'true');    // limit results to recipes with instructions
+async function recipeSearchByName(queryParams) {
 
-    // loop through search parameters and append to URL
-    for (let key in recipeSearchParams) {
-        apiQueryURL.searchParams.append(key, recipeSearchParams[key]);
-    }
+    let apiQueryURL = new URL('https://api.spoonacular.com/recipes/search'); // create URL object with recipe search API url
+    apiQueryURL.searchParams.append('apiKey', API_KEY); //add API key for auth
+    apiQueryURL.searchParams.append('limitLicense', 'true'); // limit results to those with atrribution
+    apiQueryURL.searchParams.append('number', '20'); // limit results to 20 recipes 
+    apiQueryURL.searchParams.append('instructionsRequired', 'true'); // limit results to recipes with instructions
+    apiQueryURL.searchParams.append('query', queryParams.query); // append recipe name search query
 
     // because fetch returns a promise, we add a keyword to wait until the promise is returned
     let apiResponse = await fetch(apiQueryURL);
@@ -19,3 +17,16 @@ async function recipeSearchByName(recipeSearchParams) {
 
     return apiResult; // return the result
 };
+
+async function getRecipeInformation(queryParams) {
+    let apiQueryURL = new URL('https://api.spoonacular.com/recipes/informationBulk'); // URL for recipe information bulk search
+    apiQueryURL.searchParams.append('apiKey', API_KEY); //add API key for auth
+    apiQueryURL.searchParams.append('limitLicense', 'true'); // limit results to those with atrribution
+    apiQueryURL.searchParams.append('ids', queryParams); // append CSV list of recipes 
+
+    console.log(apiQueryURL);
+        // because fetch returns a promise, we add a keyword to wait until the promise is returned
+        let apiResponse = await fetch(apiQueryURL);
+        let apiResult = await apiResponse.json(); //convert response into JSON
+        return apiResult; // return the result
+}
