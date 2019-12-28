56525852b5720032af997a66908c4028be442333
Yuriko Darragh <beljar731@gmail.com>
2019-12-28 19:48:08 +0900

    updated

--- js/winemenu.js
+++ js/winemenu.js
@@ -5,3 +5,12 @@ function onButtonClick() {
   console.log(sessionStorage.wineMenu);
   location.href="./_winesearch.html";
 }
+
+document.getElementsByName("wine-form")[0].addEventListener('submit', function(eve) {
+  eve.preventDefault();
+  let textBox = document.getElementsByName("wine_input")[0];
+  console.log(textBox.value);
+  sessionStorage.wineMenu = textBox.value;
+  console.log(sessionStorage.wineMenu);
+  location.href="./_winesearch.html";
+});
