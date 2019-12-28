8c78d1a49034fabb2cb63c239d33a970969ceed5
Melissendra <melissendra77@gmail.com>
2019-12-28 15:57:18 +0100

    carousel.js transform in vanilla js

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,25 @@
+
 const createCarousel = (container) =>{
-    const carouselSection = new HtmlElements("section", {"id": "slideshow"}, container).newElement();
-    const carouselDiv = new HtmlElements("div", {"class": "slick"}, carouselSection).newElement()   
-    const img1= new HtmlElements("div", {"class": ""}, carouselDiv).newElement();
-    const picture1 = new HtmlElements("img", {"src": "./images/Appetizer.jpg"}, img1).newElement();
-    const img2= new HtmlElements("div", {"class": ""}, carouselDiv).newElement();
-    const picture2 = new HtmlElements("img", {"src": "./images/Entree.jpg"}, img2).newElement();
-    const img3= new HtmlElements("div", {"class": ""}, carouselDiv).newElement();
-    const picture3= new HtmlElements("img", {"src": "./images/Dessert.jpg"}, img3).newElement();
+    const carouselSection = new HtmlElements("div", {"class": "container grid lg mt-2 mb-2", "id": "carousel"}, container).newElement();
+    const carouselDiv = new HtmlElements("div", {"class": "splide", "id": "splide"}, carouselSection).newElement()
+    const track = new HtmlElements("div", {"class": "splide__track"}, carouselDiv).newElement();
+    const ulElement = new HtmlElements("ul", {"class": "splide__list"}, track).newElement();
+    const liOneEl = new HtmlElements("li", {"class": "splide__slide", "width": "100%"}, ulElement).newElement();
+    const liTwoEl = new HtmlElements("li", {"class": "splide__slide", "width": "100%"}, ulElement).newElement();
+    const liThreeEl = new HtmlElements("li", {"class": "splide__slide", "width": "100%"}, ulElement).newElement();
+    const picture1 = new HtmlElements("img", {"src": "./images/Appetizer.jpg"}, liOneEl).newElement();
+    const picture2 = new HtmlElements("img", {"src": "./images/Entree.jpg"}, liTwoEl).newElement();
+    const picture3= new HtmlElements("img", {"src": "./images/Dessert.jpg"},liThreeEl).newElement();
     carousel();
 };
 
 const carousel = () =>{
-    $(document).ready(() =>{
-        $("#slideshow .slick").slick();
-    });
-};
+    new Splide("#splide", {
+        type: "loop",
+        perPage: 1,
+        autoPlay: true,
+        lazyLoad: 'nearby'
+    }).mount();
+}
+
+
